2b8993d24b83e0ef55d366695ca5b0029bb3de69
Tejas Varpe <tejasv@work.com>
2024-11-29 14:43:23 +0530

    project 2

--- src/component/RandomJoke.jsx
+++ src/component/RandomJoke.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react'
 
 function RandomJoke() {
 
-  const [joke, setJoke] = useState('');
+  const [jokes, setJokes] = useState([]);
+  const [nextJoke, setNextJoke] = useState([]);
+  const [currentJokeIndex, setCurrnetJokeIndex] = useState(0);
 
   useEffect(() => {
     const fetchJoke = async () => {
@@ -12,7 +14,8 @@ function RandomJoke() {
         throw new Error("Error....!!!");
       }
       const data = await response.json();
-      console.log(data);
+      setJokes(data.data.data)
+      console.log("List of all jokes...",jokes);
      }catch(error){
       console.log("Error! while fetching jokes...!")
      }
@@ -20,8 +23,26 @@ function RandomJoke() {
     fetchJoke();
   },[])
 
+  const renderNextJoke = () =>{
+    if(currentJokeIndex < jokes.length - 1){
+      setCurrnetJokeIndex(currentJokeIndex + 1);      
+    }else{
+      setCurrnetJokeIndex(0);
+    }
+  }
+
   return (
-    <div>RandomJoke</div>
+    <div className='joke-container'>
+      <h1>Random Joke Generator...</h1>
+      {jokes.length > 0 ? (
+        <p key={jokes[currentJokeIndex].id}>
+          {jokes[currentJokeIndex].content}
+        </p>
+      ) : (
+        <p>Loading jokes...</p>
+      )}
+      <button onClick={renderNextJoke}>Next Joke</button>
+    </div>
   )
 }
 
